b8c9286a3eb5a49d64f29edc97afd1ffd6e62cdc
Stephanie Simpson <stephsimpson911@gmail.com>
2018-01-24 00:56:45 +0000

    added page that takes in urls

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
-//stell express to use ejs as templating engince
+//tell express to use ejs as templating engine
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +24,11 @@ app.get("/urls", (req, res) => { //new route handle for /urls
   res.render("urls_index", templateVars); //pass url data to template
 });
 
+//use urls_new to render endpoint /urls/new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //new route to render single url display page
 app.get("/urls/:id", (req, res) => { //new route handle for /urls
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -28,9 +36,10 @@ app.get("/urls/:id", (req, res) => { //new route handle for /urls
 });
 
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
